98cd1c1a4f16b3cab938afdd8b3e7d812312b426
macfly <manuel@webdesignord.fr>
2016-09-21 09:55:50 +0100

    getUtm use jquery fct for doc ready and querySelectorAll

--- HTML/src/js/getUtm.js
+++ HTML/src/js/getUtm.js
@@ -4,7 +4,7 @@ var campaign  = getUtm('campaign'),
     mailArray = ['boitesSweetAlice','jardiniere','secheCheveux'];
 // fct recup argument aprés:utm_arg dans l'URL
 function getUtm(arg) {
-	// filtre la chaine location.search entre utm_ et &
+  // filtre la chaine location.search entre utm_ et &
   var regex   = new RegExp('[\?&]utm_' + arg + '=([^&]*)'),
       results = regex.exec(location.search);
   return results === null ? "utm_"+arg+" non renseigné" : results[1];
@@ -22,14 +22,17 @@ function show (element) {
   element = document.querySelectorAll(element)
   element[0].style.display = 'block';
 }
+
+$(function() {
+   // masque tous les elmnts
+   hide($('#boitesSweetAlice, #jardiniere ,#secheCheveux'));
+    // comparaison ID et utm
+    for (var i = 0; i < mailArray.length; i++) {
+      if (campaign === mailArray[i]){
+        show(mailArray[i]);
+      }
+    };
+});
 // alternative JS vanilla à jquery $(document).ready() 
-window.onload = function() {
-	// masque tous les elmnts
-	hide(document.querySelectorAll('#boitesSweetAlice, #jardiniere ,#secheCheveux'));
-	// comparaison ID et utm
-	for (var i = 0; i < mailArray.length; i++) {
-		if (campaign === mailArray[i]){
-			show(mailArray[i]);
-		}
-	}
-};
+// document.addEventListener('DOMContentLoaded', function() {
+// }, false);
